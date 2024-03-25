3f500907ad7acfe4802d389f53cbfc9ee4ef6c6b
Vijay Indalkar <61607592+vijayindalkar@users.noreply.github.com>
2024-03-25 17:24:38 +0530

    update

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -16,7 +16,6 @@ export const Signup = () => {
 
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
-      haisdhahsda
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign up"} />
@@ -32,21 +31,21 @@ export const Signup = () => {
             onChange={(e) => {
               setLastName(e.target.value);
             }}
-            placeholder="Doe"
+            placeholder="Wick"
             label={"Last Name"}
           />
           <InputBox
             onChange={(e) => {
               setUsername(e.target.value);
             }}
-            placeholder="harkirat@gmail.com"
+            placeholder="email@gmail.com"
             label={"Email"}
           />
           <InputBox
             onChange={(e) => {
               setPassword(e.target.value);
             }}
-            placeholder="123456"
+            placeholder="Password@123"
             label={"Password"}
           />
           <div className="pt-4">
