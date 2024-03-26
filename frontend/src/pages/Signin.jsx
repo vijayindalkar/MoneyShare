5dec1cf36cc023cb15d5dd134cda3e1d9ed93d65
Vijay Indalkar <61607592+vijayindalkar@users.noreply.github.com>
2024-03-26 17:41:14 +0530

    udpated signin page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,1 +1,44 @@
-export const Signin = () => {};
+import { useNavigate } from "react-router-dom";
+import { Heading } from "../components/Heading";
+import { SubHeading } from "../components/Subheading";
+import { InputBox } from "../components/InputBox";
+import { Button } from "../components/ButtonNormal";
+import { BottomWarning } from "../components/ButtonWarning";
+
+export const Signin = () => {
+  const Navigate = useNavigate();
+  
+
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className=" rounded-lg bg-white w-80 text-center p-4 h-max px-4">
+          <Heading label={"Sign in"} />
+          <SubHeading label={"Enter your credentials to access your account"} />
+          <InputBox
+            // onChange={(e) => {
+            //   setUserName(e.target.value);
+            // }}
+            placeholder={"John"}
+            label={"Email"}
+          />
+          <InputBox
+            // onChange={(e) => {
+            //   setLastName(e.target.value);
+            // }}
+            placeholder={"john@wick"}
+            label={"Password"}
+          />
+          <div className="pt-4">
+            <Button label={"Sign In"} />
+          </div>
+          <BottomWarning
+            label={"Don't have an account?"}
+            buttonText={"Sign up"}
+            to={"/signup"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -17,7 +17,7 @@ export const Signup = () => {
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
-        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+        <div className="rounded-lg bg-white w-80 text-center p-4 h-max px-4">
           <Heading label={"Sign up"} />
           <SubHeading label={"Enter your infromation to create an account"} />
           <InputBox
